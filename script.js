917a5bd67027a43f49bb16f0752aadc143129260
WAHYU OZORAH MANURUNG <125001680+wahyuozorahmanurung@users.noreply.github.com>
2024-11-19 19:47:15 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+
+// Typing Text Code
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer', 'Backend Developer', 'Blockchain Developer', 'Web Designer', 'Photographer'],
+    typeSpeed: 80,
+    backSpeed: 80,
+    backDelay: 1200,
+    loop: true,
+  });
